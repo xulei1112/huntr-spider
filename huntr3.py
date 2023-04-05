82bcaaa3fa3618cb0b125d5093424ee533f46824
NCNIPC梅苑 <64565724+xulei1112@users.noreply.github.com>
2023-04-05 14:37:23 +0800

    Update huntr3.py

--- huntr3.py
+++ huntr3.py
@@ -4,32 +4,35 @@ import time
 import csv
 
 if __name__ == '__main__':
-    path_a='e:\test.csv'
     a= open("e:/test.csv",'r')
     reader=csv.reader(a)
     rows=[row for row in reader]
     #从csv文件中取出url链接，然后使用爬虫进行访问获取
-    for i in rows:
-        print(i[0])
+    for j in rows:
+        print(j[0])
         web = Chrome()
-        web.get(i[0])
+        #打开test2.csv准备把爬取内容存入其中
+        web.get(j[0])
         #下面的element是爬取wp内容的
-        element=web.find_elements(By.ID,'write-up')
-        #截取Description内容，匹配Proof of Concept之前的内容输出
-        for i in element:
-            if "Concept" in i.text:
-                print(i.text[0:i.text.find('Proof of Concept')])
-                break
-            if "Impact" in i.text:
-                print(i.text[0:i.text.find('Impact')])
-                break
-            else:
-                print(i.text)
-        #下面的cve是爬取cve内容的
+        element=web.find_element(By.ID,'write-up')
+        #截取Description内容，匹配Proof of Concept或者impact之前的内容输出
+        ele=element.text.split('\n')
+        #print(ele[0])
+        #print(ele[4])
+        #下面的cve是爬取cve与CWE内容的
         cve=web.find_element(By.ID,'meta-container')
         l=cve.text.split('\n')
+        aa=''
+        bb=''
         for i in l:
-            if "CVE" in i:
-                print(i)
+            if "CVE-" in i:
+                #print(i)
+                aa=i
+
             if "CWE" in i:
-                print(i)
+                #print(i)
+                bb=i
+
+        with open('e:/test2.csv','a',newline='') as result:
+            writer=csv.writer(result)
+            writer.writerow([j[0],ele[0],ele[4],aa,bb])
