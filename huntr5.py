c712a841da1aea4d151bb9524483f6838192476d
NCNIPC梅苑 <64565724+xulei1112@users.noreply.github.com>
2023-04-13 11:09:56 +0800

    Update huntr5.py
    改成火狐浏览器，防止内存崩溃

--- huntr5.py
+++ huntr5.py
@@ -1,26 +1,27 @@
-from selenium.webdriver import Chrome
+from selenium.webdriver import *
 from selenium.webdriver.common.by import By
 import time
 import csv
 from selenium.webdriver.common.keys import Keys
 
 if __name__ == '__main__':
-    web = Chrome()
+    web = Firefox()
     web.get("https://huntr.dev/bounties/hacktivity/")
     #
     #for i in range(10):
     #    buttun=web.find_element(By.ID,'pagination')
     #    buttun.click()
     time.sleep(10)
-    #try finally编程提高代码稳定性，防止因为报错导致代码中断，定位到show more按钮来寻找更多的漏洞url
+    #try finally提高代码稳定性
     try:
         buttun=web.find_element(By.CSS_SELECTOR,'#hacktivity-page > div.flex.flex-col.sm\:flex-row > div.sm\:ml-4.relative.w-full.sm\:w-1\/2.mt-4.sm\:mt-0 > span > button')
-        for i in range(10000000):
+        for i in range(5000):
             buttun.click()
+        
     except:
         try:
             buttun2=web.find_element(By.CSS_SELECTOR,'#hacktivity-page > div.flex.flex-col.sm\:flex-row > div.sm\:ml-4.relative.w-full.sm\:w-1\/2.mt-4.sm\:mt-0 > span > button')
-            for i in range(10000000):
+            for i in range(5000):
                 buttun2.click()
         finally:
             print(2)
@@ -32,7 +33,7 @@ if __name__ == '__main__':
         sum+=1
     print(sum)
     f = open("e:/test.csv",'a',newline='')
-    #输出漏洞文章的url
+    #输出最新的漏洞文章的url
     for i in element:
         writer = csv.writer(f)
         writer.writerow([i.get_attribute('href')])
